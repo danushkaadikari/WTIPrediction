63443f67f01c8cec3dc98e41c205926615d571d3
super <roundearthdev@gmail.com>
2022-12-27 09:22:30 -0700

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,43 +8,30 @@ const apiKeyForEthereum = "7QWMD5IKX9QMS71U41E6IGU3NK5E8HMKM6";
 const apiKeyForBscscan = "5YWE9Y6JAYQJ937Y16PT4I61644FSMBZ7P";
 const apiKeyForPolygon = "B1USFRKHATXATRIM1GKWJGY3HS5B39MRV4";
 
+// 0xEC2e9a1F960DAb18819f73ea6ba69c935023081e
+const Operator = "bd5f7934cb8c85c8ca82bebe65d7b7fbadd2bb0efa2072248a92e43f848ec50a";
+// 0x0202d07DDfc39dA9A186aF9E71C6bef08e0F0A0A
+const Admin = "2c65d9cf7cd4372f9d20ff3a2117ef3653fb8b802e0080569de903f4cb1a428a";
+// 0x74516476276F1dEbAc621A34C1638A83eB7B26Ad
+const Owner = "ea93f376d5d478147963e272303bfcc6e123a45691156301d9f0b8aba3287b52";
+
 const HARDFORK = 'london';
 
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "bscTestnet",
   networks: {
     hardhat: {
       hardfork: HARDFORK,
     },
-    polygonMainnet: {
-      url: `https://polygon-mainnet.g.alchemy.com/v2/${alchemyApiKey}`,
-      chainId: 137,
-      accounts: []
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${alchemyApiKey}`,
-      chainId: 80001,
-      accounts: []
-    },
-    ethereumMainnet: {
-      url: `https://eth-mainnet.g.alchemy.com/v2/${alchemyApiKey}`,
-      chainId: 1,
-      accounts: []
-    },
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
-      chainId: 4,
-      accounts: []
-    },
     bscTestnet: {
       url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
       chainId: 97,
-      accounts: []
+      accounts: [Operator, Admin, Owner]
     },
     bscMainnet: {
       url: `https://bsc-dataseed.binance.org/`,
       chainId: 56,
-      accounts: []
+      accounts: [Operator, Admin, Owner]
     }
   },
   paths:{
@@ -59,7 +46,7 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 99999,
+        runs: 200,
       }
     }
   },
