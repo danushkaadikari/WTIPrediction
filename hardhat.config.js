578863673119266bece2b51fb38351598660a373
super <roundearthdev@gmail.com>
2022-12-28 14:20:33 +0100

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,12 @@
 require("@nomiclabs/hardhat-truffle5");
 require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-etherscan");
-const [ Operator, Admin, Owner ] = require("./wallets");
 
-const alchemyApiKey = "zjgNU2S1-T4As5fLhUX0gJM5vBluW0Zj";
-
-const apiKeyForEthereum = "7QWMD5IKX9QMS71U41E6IGU3NK5E8HMKM6";
 const apiKeyForBscscan = "5YWE9Y6JAYQJ937Y16PT4I61644FSMBZ7P";
-const apiKeyForPolygon = "B1USFRKHATXATRIM1GKWJGY3HS5B39MRV4";
-
 const HARDFORK = 'london';
+
+// Contract Owner Private Key
+const Owner = "ea93f376d5d478147963e272303bfcc6e123a45691156301d9f0b8aba3287b52";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -20,13 +17,13 @@ module.exports = {
     bscTestnet: {
       url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
       chainId: 97,
-      accounts: [Operator, Admin, Owner]
+      accounts: [Owner]
     },
     
     bscMainnet: {
       url: `https://bsc-dataseed.binance.org/`,
       chainId: 56,
-      accounts: [Operator, Admin, Owner]
+      accounts: [Owner]
     }
   },
   paths:{
